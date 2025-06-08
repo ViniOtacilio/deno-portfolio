4a9cb7bd6dde0e7a856421c122ed4b920a242f13
Vinicius Otacilio <viniotacilio@gmail.com>
2025-06-08 20:08:10 -0300

    commit para testar deploy

--- routes/index.tsx
+++ routes/index.tsx
@@ -62,7 +62,7 @@ interface Value {
 function Values() {
   const values: Value[] = [
     {
-      title: "Passion for Continuous Learning",
+      title: "Teste",
       description:
         "I actively seek out new technologies and stay up-to-date with industry trends.",
       image: "/img/pexels-pixabay-159711.webp",
